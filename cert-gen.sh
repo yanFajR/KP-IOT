92c3656d48223917e2b337922f433d5b242e8c28
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2021-08-01 18:05:02 +0700

    Update cert-gen.sh

--- cert-gen.sh
+++ cert-gen.sh
@@ -1,14 +1,15 @@
+CN='/CN=104.43.104.64'
 # Generate Certificate  & Key for CA
-openssl req -new -x509 -days 3650 -extensions v3_ca -keyout ca.key -out ca.crt -nodes -subj '/CN=192.168.14.100'
+openssl req -new -x509 -days 3650 -extensions v3_ca -keyout ca.key -out ca.crt -nodes -subj $CN
 
 # Generate Certificate  & Key for Server
 openssl genrsa -out server.key 2048
-openssl req -out server.csr -key server.key -new -sha256 -subj '/CN=192.168.14.100'
+openssl req -out server.csr -key server.key -new -sha256 -subj $CN
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 1826
 
 # Generate Certificate  & Key for Client
 openssl genrsa -out client.key 2048
-openssl req -out client.csr -key client.key -new -sha256 -subj '/CN=192.168.14.100'
+openssl req -out client.csr -key client.key -new -sha256 -subj $CN
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 1826
 
 # Convert to PEM
